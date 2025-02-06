3096ca9d64af72fe1d9e6d40bada26a0457045da
ecommaxs <ecommerxe01@gmail.com>
2025-02-06 09:26:04 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const loggedInUser = localStorage.getItem('loggedInUser');
+    const userIcon = document.querySelector('.user-icon');
+
+    if (userIcon) {
+        if (loggedInUser) {
+            userIcon.href = 'user.html';
+        } else {
+            userIcon.href = 'form.html';
+        }
+    } else {
+        console.error("User not found.");
+    }
+    if (advertise) {
+        if (loggedInUser) {
+            advertise.href = 'advertise.html';
+        } else {
+            adverise.href = 'form.html';
+        }
+    } else {
+        console.error("User not found.");
+    }
+    if (product) {
+        if (loggedInUser) {
+            product.href = 'product.html';
+        } else {
+            product.href = 'form.html';
+        }
+    } else {
+        console.error("User not found.");
+    }
+    if (home) {
+        if (loggedInUser) {
+            home.href = 'Home.html';
+        } else {
+            home.href = 'form.html';
+        }
+    } else {
+        console.error("User not found.");
+    }
+});
